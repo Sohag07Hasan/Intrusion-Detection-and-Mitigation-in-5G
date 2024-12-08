14b75504b94420ce051fdeb1bfc95aae4cffb45a
Sohag07Hasan <hyde.sohag@gmail.com>
2024-12-08 14:56:48 -0600

    Demo v1 Works

--- ML_IDS/data/mlids_22/config.py
+++ ML_IDS/data/mlids_22/config.py
@@ -1,19 +1,30 @@
 
 Strategy = {
     'type_1': {
+        'policy': 'attack_count',
+        'throttle_intervals': [60, 120, 180, 240],
+        'max_dl': ['3 Mbps', '2 Mbps', '1 Mbps', '500 Kbps'],
+        'max_ul': ['3 Mbps', '2 Mbps', '1 Mbps', '500 Kbps'],
+        'qos': [20, 40, 60, 80],
+        'pdu_session_deletion_limit': 250 
+    },
+
+    'type_2': {
+        'policy': 'attack_duration',
+        'throttle_intervals': [30, 60, 90, 120, 150, 180],
+        'max_dl': ['3 Mbps', '2 Mbps', '1 Mbps', '500 Kbps', '300 Kbps', '100 Kbps'],
+        'max_ul': ['3 Mbps', '2 Mbps', '1 Mbps', '500 Kbps', '300 Kbps', '100 Kbps'],
+        'qos': [20, 30, 40, 50, 60, 70],
+        'pdu_session_deletion_limit': 210 
+    },
+
+    'type_3': {
+        'policy': 'attack_count',
         'throttle_intervals': [60, 120, 180, 240, 300, 330, 360, 390, 420, 450],
         'max_dl': ['3 Mbps', '2 Mbps', '2.5 Mbps', '2 Mbps', '1.5 Mbps', '1 Mbps', '500 Kbps', '200 Kbps', '100 Kbps', '50 Kbps'],
         'max_ul': ['3 Mbps', '2 Mbps', '2.5 Mbps', '2 Mbps', '1.5 Mbps', '1 Mbps', '500 Kbps', '200 Kbps', '100 Kbps', '50 Kbps'],
         'qos': [20, 40, 60, 80, 100, 110, 120, 130, 140, 150],
         'pdu_session_deletion_limit': 500 
-    },
-
-    'type_2': {
-        'throttle_intervals': [30, 70, 100, 130],
-        'max_dl': ['5 Mbps', '3 Mbps', '1 Mbps', '500 Kbps'],
-        'max_ul': ['5 Mbps', '3 Mbps', '1 Mbps', '500 Kbps'],
-        'qos': [20, 30, 40, 50],
-        'pdu_session_deletion_limit': 150 
     },
 
 }
@@ -27,6 +38,10 @@ SavedFiles = {
 
 ##IP address of UEs
 MS_IP_POOL = ['10.60.0.0/24', '10.61.0.0/24', '10.62.0.0/24']
+
+## Defining File Name
+ATTACK_RECORD = './attack_records.csv'
+
 
 ## Return the strategy based on tye
 def get_enforcment_strategy(type='type_1'):
